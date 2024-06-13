7d07e4b99913916584b2e239eac2fd4c8c67a9f0
12anas_rookie12 <skanas0312@gmail.com>
2024-06-13 17:51:20 +0530

    yess

---
+++ start.js
@@ -0,0 +1,103 @@
+// defining variables by using "var" and "let"
+// let first = "jerk";
+// console.log(first);
+// var sec = "joke";
+// console.log(sec);
+// defining alert message
+// let Alert = "Muzammil kya h?";
+// alert(Alert);
+
+// =================== Question type========================
+// Declaring two variables : Admin and Name.
+// Assign the Value "John" to Name.
+// Copy the value of from name and paste to Admin.
+// Show the value of Admin using alert (must output "Admin")
+
+// let Admin;
+// let Name;
+// name="Bhadwa!!!";
+// Admin=name;
+// alert(Admin)
+
+// let ff = "bail bhi h!!";
+// alert(ff);
+
+// =========================Understanding Constants==========================
+// const birthday="03.12.2004";
+// birthdy="13.12.2004"
+// console.log(birthday);
+
+// =======================Data types in JS====================================
+
+// 1) Number:
+// let n1 = 12;
+// let n2 = 1.22;
+// console.log(n1,n2)
+
+// 2) Bigint : It's nthng but the number which have laarge integer (might indefinite)
+// let bigint = 1111111111111111n;
+// console.log(bigint)
+
+// 3) String :
+// let norm = "str";
+// let int = "1 + 2";
+// let single ='Single quotes are ok toooooo'
+// console.log(norm)
+// console.log(int)
+// console.log(single)
+
+// =======================We can embedded through String==============================
+
+// let s1 = `${norm} is a text type`;
+// console.log(s1);
+// console.log(`the answer of 1 + 3 = ${1+3}`);
+
+// 4) Boolean :
+// let Greater = 4<1;
+// alert(Greater);
+
+// 5) Null:
+// let age = null;
+// alert(age);
+
+// 6) Undefine:
+// let data;   
+// alert(data);
+
+// 7) Object (Uses for more Complex Data Structures)
+// 8) Symbol (Uses for Unique Identifiers)
+
+// ==========================Basic Operators=============================
+
+// Basic operators 
+
+// addition 
+// let apples = "2";
+// let oranges = "3";
+// alert( apples + oranges );
+
+// 1.arithmatic operator 
+// a1 = 20;
+// a2 = 30;
+// a3 = a1 + a2;
+// console.log(a3);
+
+// 2.assignment operator 
+// a1 = 20;  //a1 = 20 + 30
+// a1 += 30;
+// console.log(a1);
+
+// let n = 2;
+// n += 5; // now n = 7 (same as n = n + 5)
+// n *= 2; // now n = 14 (same as n = n * 2)
+
+// alert( n ); // 14
+
+// let x = 1;
+// x = -x;
+// alert( x ); //  unary negation was applied
+
+// let z = 36, y = 35;
+// alert( z + 50 ); 
+
+
