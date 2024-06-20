5a065128b318d74b27d2288afd697b23bcfafdaf
12anas_rookie12 <skanas0312@gmail.com>
2024-06-20 17:39:44 +0530

    yess

--- start.js
+++ start.js
@@ -99,5 +99,103 @@
 
 // let z = 36, y = 35;
 // alert( z + 50 ); 
+// division  8 / 3
+// alert( 8 % 3 ); 
+
+// exponentional 2² 
+// alert( 2 ** 2 ); // 2² = 4
+// alert( 2 ** 3 ); // 2³ = 8
+// alert( 2 ** 4 ); // 2⁴ = 16
+
+// '1' is string ...therefore value is 12
+// alert( '1' + 2 ); // "12"
+// alert( 2 + '1' ); // "21"
+
+// alert(2 + 2 + '1' ); 
+
+// 3.string operator 
+// let s = "my" + "string";
+// alert(s); // mystring
+
+// 4.increment and decement operator 
+// ++x pre-increment ...increment x by 1 then return x 
+// x++ post-increment ...return x than increment by 1
+// --x pre-decrement ...decrement x by 1 then return x
+// x-- post-decrement ...return x than decrement by 1
+
+// let f;
+// f = 10;
+// console.log(++f);  //11
+// console.log(f);     //11
+
+// let g;
+// g = 12;
+// console.log(g++);  //12
+// console.log(g);     //13
+
+// let h;
+// h = 10;
+// console.log(--h);  //9
+// console.log(h);     //9
+
+// let i;
+// i = 12;
+// console.log(i--); //12
+// console.log(i);  //11
+
+// 5.logical operator
+// && Logical AND: true if both the operands/boolean values are true, else evaluates to false
+// || Logical OR: true if either of the operands/boolean values is true. evaluates to false if both are false
+// ! Logical NOT: true if the operand is false and vice-versa.
+
+
+// AND && 
+const a = true, b = false;
+// const c = 4;
+// console.log(a && a); // true
+// console.log(a && b);  // false
+// console.log((c > 2) && (c < 2)); // false
+
+// || OR 
+// const a = true, b = false, c = 4;
+// console.log(a || b); // true
+// console.log(b || b); // false
+// console.log((c>2) || (c<2)); // true
+
+// ! NOT 
+// const a = true, b = false;
+// console.log(!a); // false
+// console.log(!b); // true
+
+// url 
+// https://javascript.info/operators
+
+// 6. comparison operator 
+// x=5
+// x =="5"  //true
+// x === "5" //false
+
+// url 
+// https://www.w3schools.com/js/js_comparisons.asp
+
+// 7.condition operator 
+let company = prompt('which company created javascript?');
+if(company == 'nestscape'){
+  alert("you are right");
+}else{
+  alert("you are wrong");
+}
+
+company == 'nestscape' ? console.log("you are right") :  console.log("you are wrong");
+
+// functions 
+// url 
+// https://javascript.info/function-basics
+
+
+// Array 
+// const fruites = ["Bannana", "Mango", "Orange", "Apple"];
+//       index..... 0           1         2         3
+// length 4
 
 
